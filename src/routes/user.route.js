2af2c7273e647e1f0f9fe146da2522f749b5a939
Angelinador <tonyperezpz@gmail.com>
2025-11-03 01:20:36 -0600

    info_error

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -12,6 +12,14 @@ const {
 } = require("../middlewares/validarCorreo.middleware")
 const rateLimit = require("express-rate-limit");
 
+app.set("trust proxy", 1); // Confía en el primer proxy (por ejemplo Cloudflare o Nginx)
+/*
+    express-rate-limit usa la IP del cliente (req.ip) para contar solicitudes.
+    Si la app corre detrás de un proxy o túnel, Express recibe una IP "falsa" del proxy (por ejemplo, 127.0.0.1).
+    El proxy envía la IP real del cliente en el header X-Forwarded-For.
+    Pero Express no confía en ese header por defecto (trust proxy = false).
+    Entonces express-rate-limit detecta que hay un X-Forwarded-For, pero no puede usarlo → lanza ese error.
+*/
 const codeLimiter = rateLimit({
     windowMs: 10 * 60 * 1000, // Cada 10 min
     max: 3, // 3 intentos por IP
