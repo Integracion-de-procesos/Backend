84133cd40eb5049fdf609fcc4ed1577e963cde3e
Angelinador <tonyperezpz@gmail.com>
2025-11-02 18:18:22 -0600

    limitador de correos

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -10,11 +10,21 @@ const {
     enviarCodigo,
     verificarCodigo
 } = require("../middlewares/validarCorreo.middleware")
-const express = require("express");
+const rateLimit = require("express-rate-limit");
 
+const codeLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // Cada 10 min
+    max: 3, // 3 intentos por IP
+    message: {
+        success: false,
+        message: "Demasiadas solicitudes de verificación, intenta más tarde",
+    },
+});
+
+const express = require("express");
 const router = express.Router();
 
-router.post("/enviar", enviarCodigo);
+router.post("/enviar", codeLimiter, enviarCodigo);
 router.post("/", verificarCodigo, crearUsuario);
 router.get("/", encontrarUsuarios);
 router.get("/id", encontrarIdUsuario);
